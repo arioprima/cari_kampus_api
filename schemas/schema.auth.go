c94fe58c438cd2aea41f9ecd1bd8035a43031d4b
arioprima <arioprima2@gmail.com>
2024-06-19 22:25:26 +0700

    feat: create schema auth"

---
+++ schemas/schema.auth.go
@@ -0,0 +1,13 @@
+package schemas
+
+import "time"
+
+type SchemaAuth struct {
+	ID string `json:"id" validate:"uuid"`
+	Password string `json:"password" validate:"required,min=3,max=100"`
+	Nama string `json:"nama" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+	RoleId string `json:"role_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
